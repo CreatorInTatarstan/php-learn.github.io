f3866df63d7116ea2dad19b486400b2a03a1b834
CreatorInTatarstan <164174552+CreatorInTatarstan@users.noreply.github.com>
2024-11-07 09:02:25 +0300

    Add files via upload

---
+++ php.js
@@ -0,0 +1,4 @@
+document.getElementById('enterBtn').addEventListener('click', function() {
+    document.getElementById('intro').style.display = 'none';
+    document.getElementById('mainContent').style.display = 'block';
+});
